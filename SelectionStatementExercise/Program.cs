64e1feda740021c9c35de3af02f236a217b36be7
Adam Jankowski <ghost17814@gmail.com>
2023-04-12 13:17:05 -0400

    Yippy kiyay!

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10000000);
+            Console.WriteLine("Bet you can't guess my favorite number :p");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("...luck guess, I'd play the lottery if I were you");
+
+            }
+
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("not even close... way too low");
+
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("looking high to me");
+
+            }
+            else Console.WriteLine("ooga booga");
+
+
+            }
         }
-    }
-}
+    } 
